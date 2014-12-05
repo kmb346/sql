d549d052f3fdb394fcf27e2186a391ae061ce14f
Behrens Kevin <kmb346@yahoo.com>
2014-12-05 18:52:02 +0900

    message

--- sqla.py
+++ sqla.py
@@ -1,18 +1,22 @@
-# Create a SQLite3 database and table
+# JOINing data from multiple tables - cleanup
 
 
-# import the sqlite3 library
 import sqlite3
-# create a new database if the database doesn't already exist
-conn  = sqlite3.connect("new.db")
 
-# get a cursor object used to execute SQL commands
-cursor = conn.cursor()
-
-# create a table
-cursor.execute("""CREATE TABLE population
-				(city TEXT, state TEXT, population INT)
-				""")
-
-# close the connection
-conn.close()
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+	
+	# retrieve data
+    c.execute("""SELECT population.city, population.population,
+	          regions.region FROM population, regions
+			  WHERE population.city = regions.city ORDER BY
+			  population.city ASC""")
+			  
+    rows = c.fetchall()
+	
+    for r in rows:
+        print "City:  " + r[0]
+        print "Population:  " + str(r[1])
+        print "Region:  " + r[2]
+        print 
+		
--- sqlc.py
+++ sqlc.py
@@ -6,13 +6,36 @@ import sqlite3
 with sqlite3.connect("new.db") as connection:
     c = connection.cursor()
 	
-    # insert multiple records using a tuple
+    c.execute("""CREATE TABLE regions
+              (city TEXT, region TEXT)
+              """)
+
     cities = [
-	         ('Boston', 'MA', 600000),
-             ('Chicago', 'IL', 2700000),
-			 ('Houston', 'TX', 2100000),
-			 ('Phoenix', 'AZ', 1500000)
-			 ]
-			 
-    # insert data into table
-    c.executemany('INSERT INTO population VALUES(?, ?, ?)', cities)
+              ('New York City', 'Northeast'),
+              ('San Francisco', 'West'),
+              ('Chicago', 'Midwest'),
+              ('Houston', 'South'),
+			  ('Phoenix', 'West'),
+			  ('Boston', 'Northeast'),
+			  ('Los Angeles', 'West'),
+			  ('Houston', 'South'),
+			  ('Philadelphia', 'Northeast'),
+			  ('San Antonio', 'South'),
+			  ('San Diego', 'West'),
+			  ('Dallas', 'South'),
+			  ('San Jose', 'West'),
+			  ('Jacksonville', 'South'),
+			  ('Indianapolis', 'Midwest'),
+			  ('Austin', 'South'),
+			  ('Detroit', 'Midwest')
+			  ]
+			  
+    c.executemany("INSERT INTO regions VALUES(?, ?)", cities)
+	
+    c.execute("SELECT * FROM regions ORDER BY region ASC")
+	
+    rows = c.fetchall()
+	
+    for r in rows:
+        print r[0], r[1]
+    
